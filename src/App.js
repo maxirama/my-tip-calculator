4bc7e987e3b065920fd588879db2c5b63273b445
maxirama <maximilianogastonrama@gmail.com>
2021-09-30 18:18:57 -0300

    Add the calculations logic and few components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const Input = ({onChange, name, value}) => {
+  return (
+    <>
+      <label for={name}>{name.toUpperCase()}</label>
+      <input type='text' name={name} onChange={onChange} value={value}/>
+    </>
+  )
+}
 
 function App() {
+  const initialValues = {
+    bill: 0,
+    tip: 0,
+    people: 1,
+    }
+    const [values , setValues] = useState(initialValues);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Input name='bill' onChange={(e=>{setValues({...values, bill: parseFloat(e.target.value)})})} value={values.bill} />
+      <Input name='tip' onChange={(e=>{setValues({...values, tip: parseFloat(e.target.value)})})} value={values.tip} />
+      <Input name='people' onChange={(e=>{setValues({...values, people: parseInt(e.target.value)})})} value={values.people} />
+      {/* <Total values={values} /> */}
+      <div>
+          <p> Tip per person : {values.bill * (0.01 * values.tip) / values.people}</p>
+          <p> Total per person: {values.bill / values.people} </p>
+          <button onClick={(e=>{setValues(initialValues)})}> Reset </button>
+      </div>
+
+    </>
+  )
 }
 
 export default App;
